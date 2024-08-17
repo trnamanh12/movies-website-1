ade724bdb9cb589215cd18686ba63e20ded49499
trnamanh12 <trnamanh8386@gmail.com>
2024-08-17 21:06:27 +0700

    Add: custom user form

---
+++ app/user/forms.py
@@ -0,0 +1,12 @@
+# app/user/forms.py
+from django import forms
+from django.contrib.auth.models import User
+
+class CustomUserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'date_joined']
+        widgets = {
+            'date_joined': forms.DateInput(attrs={'readonly': 'readonly'}),
+            
+        }
