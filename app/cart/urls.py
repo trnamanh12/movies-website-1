2b346371520df5d68d47279ec9ae151464934b8b
trnamanh12 <trnamanh8386@gmail.com>
2024-08-20 16:12:57 +0700

    add: '/cart' to mysite/urls.py, and fix: add_to_cart() function, and app/cart/urls.py

--- app/cart/urls.py
+++ app/cart/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('add-to-cart/<int:ticket_id>/', add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
-    path('view-cart/', view_cart, name='view_cart'),
+    path('view_cart/', view_cart, name='view_cart'),
     path('checkout/', checkout, name='checkout'),
     
 ]
--- app/cart/views.py
+++ app/cart/views.py
@@ -2,24 +2,32 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Cart, CartItem, Payment
 from movies.models import Ticket
+from django.http import HttpResponse, HttpRequest
+from django.contrib import messages
 
 @login_required
 def add_to_cart(request, ticket_id):
     ticket = Ticket.objects.get(id=ticket_id)
     cart, created = Cart.objects.get_or_create(user=request.user)
     cart_item, created = CartItem.objects.get_or_create(cart=cart, ticket=ticket)
-    if not created:
+    
+    if created:
+        cart_item.quantity = 1
+    else:
         cart_item.quantity += 1
-        # ticket.quantity = cart_item.quantity
-        cart_item.save()
+    
+    cart_item.save()
     return redirect('view_cart')
 
 @login_required
 def view_cart(request):
-    cart = Cart.objects.get(user=request.user)
-    cart_items = CartItem.objects.filter(cart=cart).select_related('ticket')
-    total_amount = cart.get_total_amount()
-    return render(request, 'cart/view_cart.html', {'cart_items': cart_items, 'total_amount': total_amount})
+    try:
+        cart = Cart.objects.get(user=request.user)
+        cart_items = CartItem.objects.filter(cart=cart).select_related('ticket')
+        total_amount = cart.get_total_amount
+        return render(request, 'cart/view_cart.html', {'cart_items': cart_items, 'total_amount': total_amount})
+    except Cart.DoesNotExist:
+        return render(request, 'cart/no_cart.html')
 
 @login_required
 def remove_from_cart(request, item_id):
@@ -30,9 +38,10 @@ def remove_from_cart(request, item_id):
 @login_required
 def checkout(request):
     cart = Cart.objects.get(user=request.user)
-    total_amount = cart.get_total_amount()
-    payment = Payment.objects.create(user=request.user, cart=cart, amount=total_amount, status='Pending')
+    total_amount = cart.get_total_amount
+    payment = Payment.objects.create(user=request.user, cart=cart, status='Pending')
     # Simulate payment processing
+    messages.info(request, 'Payment processing...')
     payment.status = 'Completed'  # Simulating a successful payment
     payment.save()
     # Clear the cart after payment
