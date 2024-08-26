a901dd5a494806e75fc2db5c1557b5f21cd72ce3
trnamanh12 <trnamanh8386@gmail.com>
2024-08-26 22:27:30 +0700

    update: user_login redirect and movies_detail.html

--- app/user/views.py
+++ app/user/views.py
@@ -46,7 +46,7 @@ def user_login(request):
         user = authenticate(request, username=username,password=password)
         if user is not None:
             login(request, user)
-            return redirect('profile')
+            return redirect('home')
         else:
             messages.error(request, 'Invalid username or password.')
     return render(request, 'login.html')
